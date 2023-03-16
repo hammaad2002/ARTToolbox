b14189cd274b46e6762da17b5cbd439167d0a4fa
hammaad2002 <hammadalikhan2002@gmail.com>
2023-03-17 01:52:02 +0500

    try 80...

--- wav2vec2ModelWrapper.py
+++ wav2vec2ModelWrapper.py
@@ -185,7 +185,7 @@ class wav2vec2Model(PytorchSpeechRecognizerMixin, SpeechRecognizerMixin, PyTorch
         #model's dictionary
         bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
         # Changing the variable name for my convenience 
-        x_tensor = masked_adv_input.to(self._device)
+        x_tensor = x.to(self._device)
         x_tensor = x_tensor.float()
         # Performing inference
         self.__model.cuda()
