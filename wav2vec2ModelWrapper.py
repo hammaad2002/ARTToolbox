38b6458b623ccf76acba82aefee4c44382ab5170
hammaad2002 <hammadalikhan2002@gmail.com>
2023-03-16 21:08:23 +0500

    try 58...

--- wav2vec2ModelWrapper.py
+++ wav2vec2ModelWrapper.py
@@ -143,7 +143,7 @@ class wav2vec2Model(PytorchSpeechRecognizerMixin, SpeechRecognizerMixin, PyTorch
         import torch
         print(x)
         print(y)
-        audioo = torch.from_numpy(x).clone().requires_grad_(True).to(self.device)
+        audioo = torch.from_numpy(x).clone().requires_grad_(True).retain_grad().to(self.device)
         #freeze model's weights
         print(audioo)
         self.__model.eval()
