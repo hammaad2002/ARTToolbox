1357580a3ab1dc621cce1c11b02eb785e506e7bb
hammaad2002 <hammadalikhan2002@gmail.com>
2023-03-23 01:35:11 +0500

    reverted the changes I made recently...

--- adversarial_asr.py
+++ adversarial_asr.py
@@ -58,25 +58,11 @@ class CarliniWagnerASR(ImperceptibleASR):
         self,
         estimator: wav2vec2Model,
         eps: float = 2000.0,
-        max_iter: int = 1000,
         learning_rate: float = 100.0,
-
-        optimizer: Optional["torch.optim.Optimizer"] = None,
-        global_max_length: int = 20000,
-        initial_rescale: float = 1.0,
-
-        
+        max_iter: int = 1000,
         decrease_factor_eps: float = 0.8,
         num_iter_decrease_eps: int = 10,
-        max_num_decrease_eps: Optional[int] = None,
-
-        targeted: bool = True,
-        train_mode_for_backward: bool = True,
-        clip_min: Optional[float] = None,
-        clip_max: Optional[float] = None,
-        const: float = 1.0,
-
-        batch_size: int = 1,
+        batch_size: int = 16,
     ):
         """
         Create an instance of the :class:`.CarliniWagnerASR`.
@@ -91,54 +77,24 @@ class CarliniWagnerASR(ImperceptibleASR):
         # pylint: disable=W0231
 
         # re-implement init such that inherited methods work
-        EvasionAttack.__init__(self, estimator = estimator)
+        EvasionAttack.__init__(self, estimator=estimator)  # pylint: disable=W0233
+        self.masker = None  # type: ignore
+        self.eps = eps
+        self.learning_rate_1 = learning_rate
+        self.max_iter_1 = max_iter
+        self.max_iter_2 = 0
+        self._targeted = True
+        self.decrease_factor_eps = decrease_factor_eps
+        self.num_iter_decrease_eps = num_iter_decrease_eps
+        self.batch_size = batch_size
 
-        super(CarliniWagnerASR, self).__init__(
-        estimator = estimator,
-        eps = eps,
-        max_iter_1 = max_iter,
-        max_iter_2 = 0,
-        learning_rate_1 = learning_rate,
-        optimizer_1 = optimizer,
-        targeted = True,
-        global_max_length = global_max_length,
-        initial_rescale = initial_rescale,
-        decrease_factor_eps = decrease_factor_eps,
-        num_iter_decrease_eps = num_iter_decrease_eps,
-        max_num_decrease_eps = max_num_decrease_eps,
-        train_mode_for_backward = train_mode_for_backward,
-        clip_min = clip_min,
-        clip_max = clip_max,
-        )
-        self.rec_const = 1./const if const is not None else 0.
+        # set remaining stage 2 params to some random values
+        self.alpha = 0.1
+        self.learning_rate_2 = 0.1
+        self.loss_theta_min = 0.0
+        self.increase_factor_alpha: float = 1.0
+        self.num_iter_increase_alpha: int = 1
+        self.decrease_factor_alpha: float = 1.0
+        self.num_iter_decrease_alpha: int = 1
 
-    def _forward_1st_stage(
-        self,
-        original_input: np.ndarray,
-        original_output: np.ndarray,
-        local_batch_size: int,
-        local_max_length: int,
-        rescale: np.ndarray,
-        input_mask: np.ndarray,
-        real_lengths: np.ndarray,
-    ) -> Tuple["torch.Tensor", "torch.Tensor", np.ndarray, "torch.Tensor", "torch.Tensor"]:
-
-        import torch
-
-        # Compute perturbed inputs
-        local_delta = self.global_optimal_delta[:local_batch_size, :local_max_length]
-        local_delta_rescale = torch.clamp(local_delta, -self.eps, self.eps).to(self.estimator.device)
-        local_delta_rescale *= torch.tensor(rescale).to(self.estimator.device)
-        adv_input = local_delta_rescale + torch.tensor(original_input).to(self.estimator.device)
-        masked_adv_input = adv_input * torch.tensor(input_mask).to(self.estimator.device)
-
-        # Compute loss and decoded output
-        loss, decoded_output = self.estimator.compute_loss_and_decoded_output(
-            masked_adv_input=masked_adv_input,
-            original_output=original_output,
-            real_lengths=real_lengths,
-        )
-
-        loss = loss + self.reg_const * torch.norm(local_delta_rescale)
-        self.estimator.eval()
-        return loss, local_delta, decoded_output, masked_adv_input, local_delta_rescale
+        self._check_params()
